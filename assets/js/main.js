107cbe852f96f9dd5f18e0bb1bfdfed2d02b1062
jsmuji <160035977+jsmuji@users.noreply.github.com>
2024-12-06 14:14:01 +0800

    Page 2
    Starting page 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,24 +20,26 @@
 		$(document).ready(function() {
 			const imageWrapper = $('.image-wrapper');
 	
-			// Intersection Observer to trigger vertical bar animation on scroll
+
 			const observer = new IntersectionObserver((entries, observer) => {
 				entries.forEach(entry => {
 					if (entry.isIntersecting) {
-						// Add class to start animation when element is in view
+
 						entry.target.classList.add('animate-bars');
-						observer.unobserve(entry.target); // Stop observing once animation starts
+						observer.unobserve(entry.target); 
 					}
 				});
 			}, {
-				threshold: 0.5 // Trigger when 50% of the element is visible
+				threshold: 0.5 
 			});
 	
-			// Observe the image-wrapper for scroll visibility
+
 			imageWrapper.each(function() {
 				observer.observe(this);
 			});
 		});
+
+
 	// Play initial animations on page load.
 		$window.on('load', function() {
 			window.setTimeout(function() {
